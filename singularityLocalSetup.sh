9a901589dfabe4351d9fe5f54d4336a121bd1bc2
darlenewagner <ydn3@cdc.gov>
2025-04-29 11:35:19 -0400

    Prepare to leverage Singularity

---
+++ singularityLocalSetup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+singularity pull https://depot.galaxyproject.org/singularity/perl:5.32
+
+singularity build my_perl.sif perl\:5.32
+
+singularity pull https://depot.galaxyproject.org/singularity/fastx_toolkit:0.0.14--h503566f_12
+
+singularity build my_fastx_toolkit.sif fastx_toolkit:0.0.14--h503566f_12
+
+singularity pull https://depot.galaxyproject.org/singularity/prinseq:0.20.4--hdfd78af_5
+
+singularity build my_prinseq.sif prinseq:0.20.4--hdfd78af_5
